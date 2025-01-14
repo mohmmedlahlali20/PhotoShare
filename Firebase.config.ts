39b404c4037fe9bec8c8370e4f87b63c33cc173e
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-14 22:44:18 +0100

    feat(firebase): add Firebase configuration

---
+++ Firebase.config.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6kCXbC2RWA-Y53VvwIE-SFt5r3dmORJI",
+  authDomain: "photoshare-811b7.firebaseapp.com",
+  projectId: "photoshare-811b7",
+  storageBucket: "photoshare-811b7.firebasestorage.app",
+  messagingSenderId: "781248521295",
+  appId: "1:781248521295:web:8b871328e9040cf68f763a",
+  measurementId: "G-1E3LRHEYJZ"
+};
+
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP)
+export const FIREBASE_ANALYTICS = getAnalytics(FIREBASE_APP);
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP)
